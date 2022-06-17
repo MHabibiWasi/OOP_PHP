a2ea0d803f265885e0a441f186d66281b278f601
MHabibiWasi <habibiunyila@gmail.com>
2022-06-18 00:04:26 +0700

    gege

--- pertemuan02/index.php
+++ pertemuan02/index.php
@@ -5,6 +5,11 @@ class Produk
     $penulis = "penulis",
     $penerbit = "penerbit",
     $harga = 0;
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
 }
 // $produk1 = new Produk();
 // $produk1->judul = "Naruto";
@@ -21,6 +26,16 @@ $produk3->judul = "Naruto";
 $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
-var_dump($produk3);
 
-echo "Komik : $produk3->penulis, $produk3->penerbit";
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
